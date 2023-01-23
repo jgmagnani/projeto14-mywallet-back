73d268b8ee299d120082ad4fc08541fd3a72e8d4
jgmagnani <jogamabnu@gmail.com>
2023-01-23 18:00:53 -0300

    iniciado

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors"
+
+const server = express();
+
+server.use(cors())
+
+server.listen(4002, () => {
+    console.log("Meu server ta rodando!!!")
+
+addEventListener
+
+}
+)
+
+export function testar(){
+    return (
+       
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    )
+}
